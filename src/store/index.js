4978690b676a5c473ff8f825ce6dccfe1b553127
juningwei <289704828@qq.com>
2022-05-09 16:24:31 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import createPersistedstate from "vuex-persistedstate";
+import cart from "./modules/cart";
+import user from "./modules/user";
+import category from "./modules/category";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
-})
+    cart,
+    user,
+    category,
+  },
+  plugins: [
+    createPersistedstate({
+      key: "erabbit-client-pc-store",
+      paths: ["user", "cart"],
+    }),
+  ],
+});
