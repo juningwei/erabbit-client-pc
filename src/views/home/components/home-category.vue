adeee06f85281ad3a19809e4d7ba884efd2c2864
juningwei <289704828@qq.com>
2022-05-11 21:03:01 +0800

    左侧分类弹层展示

--- src/views/home/components/home-category.vue
+++ src/views/home/components/home-category.vue
@@ -1,56 +1,86 @@
 <template>
-  <div class='home-category'>
+  <div class="home-category">
     <ul class="menu">
-      <li v-for="item in menuList" :key="item.id">
-        <RouterLink :to="`/category/${item.id}`">{{item.name}}</RouterLink>
+      <li
+        v-for="item in menuList"
+        :key="item.id"
+        @mouseenter="categoryId = item.id"
+      >
+        <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         <template v-if="item.children">
           <RouterLink
             v-for="sub in item.children"
             :key="sub.id"
-            :to="`/category/sub/${sub.id}`">
-            {{sub.name}}
+            :to="`/category/sub/${sub.id}`"
+          >
+            {{ sub.name }}
           </RouterLink>
         </template>
       </li>
     </ul>
+
+    <!-- 弹层 -->
+    <div class="layer">
+      <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
+      <ul
+        v-if="currCategory && currCategory.goods && currCategory.goods.length"
+      >
+        <li v-for="item in currCategory.goods" :key="item.id">
+          <RouterLink to="/">
+            <img :src="item.picture" alt="" />
+            <div class="info">
+              <p class="name ellipsis-2">{{ item.name }}</p>
+              <p class="desc ellipsis">{{ item.desc }}</p>
+              <p class="price"><i>¥</i>{{ item.price }}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import { useStore } from 'vuex'
-import { reactive, computed } from 'vue'
+import { useStore } from "vuex";
+import { reactive, computed, ref } from "vue";
 
-export default{
-  setup () {  
+export default {
+  setup() {
     const brand = reactive({
-      id: 'brand',
-      name: '品牌',
-      children: [{ id: 'brand-chilren', name: '品牌推荐' }]
-    })
-    
-    const store = useStore()
-    const menuList = computed(() =>{
-      const list = store.state.category.list.map(item => {
+      id: "brand",
+      name: "品牌",
+      children: [{ id: "brand-chilren", name: "品牌推荐" }],
+    });
+
+    const store = useStore();
+    const menuList = computed(() => {
+      const list = store.state.category.list.map((item) => {
         return {
           id: item.id,
           name: item.name,
           // 防止初始化没有children的时候调用slice函数报错
-          children: item.children && item.children.slice(0, 2)
-        }
-      })
-      list.push(brand)
-      return list
-    })
-    return { menuList }
-  }
-}
+          children: item.children && item.children.slice(0, 2),
+           goods: item.goods
+        };
+      });
+      list.push(brand);
+      return list;
+    });
+    // 获取当前分类逻辑
+    const categoryId = ref(null);
+    const currCategory = computed(() => {
+      return menuList.value.find((item) => item.id === categoryId.value);
+    });
+    return { menuList, categoryId, currCategory };
+  },
+};
 </script>
 
-<style scoped lang='less'>
+<style scoped lang="less">
 .home-category {
   width: 250px;
   height: 500px;
-  background: rgba(0,0,0,0.8);
+  background: rgba(0, 0, 0, 0.8);
   position: relative;
   z-index: 99;
   .menu {
@@ -66,6 +96,79 @@ export default{
         color: #fff;
         &:first-child {
           font-size: 16px;
+        }
+      }
+    }
+  }
+  &:hover {
+    .layer {
+      display: block;
+    }
+  }
+  .layer {
+    width: 990px;
+    height: 500px;
+    background: rgba(255, 255, 255, 0.8);
+    position: absolute;
+    left: 250px;
+    top: 0;
+    display: none;
+    padding: 0 15px;
+    h4 {
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 80px;
+      small {
+        font-size: 16px;
+        color: #666;
+      }
+    }
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      li {
+        width: 310px;
+        height: 120px;
+        margin-right: 15px;
+        margin-bottom: 15px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        background: #fff;
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+        a {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          padding: 10px;
+          &:hover {
+            background: #e3f9f4;
+          }
+          img {
+            width: 95px;
+            height: 95px;
+          }
+          .info {
+            padding-left: 10px;
+            line-height: 24px;
+            width: 190px;
+            .name {
+              font-size: 16px;
+              color: #666;
+            }
+            .desc {
+              color: #999;
+            }
+            .price {
+              font-size: 22px;
+              color: @priceColor;
+              i {
+                font-size: 16px;
+              }
+            }
+          }
         }
       }
     }
