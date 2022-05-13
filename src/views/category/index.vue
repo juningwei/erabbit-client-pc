b164e7f64c1164934c6cbe1a7150e2aa8cf55641
juningwei <289704828@qq.com>
2022-05-13 19:51:26 +0800

    分类商品布局

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -21,6 +21,17 @@
         </ul>
       </div>
       <!-- 不同分类商品 -->
+      <!-- 分类关联商品 -->
+      <div class="ref-goods">
+        <div class="head">
+          <h3>- 海鲜 -</h3>
+          <p class="tag">温暖柔软，品质之选</p>
+          <XtxMore />
+        </div>
+        <div class="body">
+          <GoodsItem v-for="i in 5" :key="i" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,8 +40,12 @@ import { findBanner } from '@/api/home'
 import { ref,computed } from 'vue'
 import { useStore } from 'vuex'
 import { useRoute } from 'vue-router'
+import GoodsItem from './components/goods-item'
 export default {
   name: 'TopCategory',
+  components:{
+    GoodsItem
+  },
   setup () {
     // 轮播图
     const sliders = ref([])
@@ -92,6 +107,31 @@ export default {
           }
         }
       }
+    }
+  }
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+    .body {
+      display: flex;
+      justify-content: flex-start;
+      flex-wrap: wrap;
+      padding: 0 65px 30px;
     }
   }
 }
