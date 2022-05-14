a9734c3543583e545aa822697ad006490299c9a7
juningwei <289704828@qq.com>
2022-05-14 16:19:31 +0800

    商品列表

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -8,7 +8,14 @@
         <!-- 排序 -->
         <SubSort />
         <!-- 列表 -->
+        <ul>
+          <li v-for="i in 20" :key="i" >
+            <GoodsItem :goods="{}" />
+          </li>
+        </ul>
       </div>
+
+      
       
     </div>
   </div>
@@ -18,10 +25,32 @@
 import SubBread from './components/sub-bread'
 import SubFilter from './components/sub-filter'
 import SubSort from './components/sub-sort'
+import GoodsItem from './components/goods-item'
+
 export default {
-  components: { SubBread, SubFilter, SubSort},
+  components: { SubBread, SubFilter, SubSort, GoodsItem},
   setup() {
     
   },
 }
 </script>
+
+<style scoped lang='less'>
+.goods-list {
+  background: #fff;
+  padding: 0 25px;
+  margin-top: 25px;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 5px;
+    li {
+      margin-right: 20px;
+      margin-bottom: 20px;
+      &:nth-child(5n) {
+        margin-right: 0;
+      }
+    }
+  }
+}
+</style>
