b2fa8360242ea99ac7165b00999a930a18316ff6
juningwei <289704828@qq.com>
2022-05-18 21:48:02 +0800

    评价列表结构

--- src/views/goods/components/goods-comment.vue
+++ src/views/goods/components/goods-comment.vue
@@ -22,7 +22,37 @@
       </div>
     </div>
     <!-- 排序 -->
-    <div class="sort" v-if="commentInfo"></div>
+    <div class="sort">
+      <span>排序：</span>
+      <a href="javascript:;" class="active">默认</a>
+      <a href="javascript:;">最新</a>
+      <a href="javascript:;">最热</a>
+    </div>
+     <!-- 列表 -->
+    <div class="list">
+      <div class="item">
+        <div class="user">
+          <img src="http://zhoushugang.gitee.io/erabbit-client-pc-static/uploads/avatar_1.png" alt="">
+          <span>兔****m</span>
+        </div>
+        <div class="body">
+          <div class="score">
+            <i class="iconfont icon-wjx01"></i>
+            <i class="iconfont icon-wjx01"></i>
+            <i class="iconfont icon-wjx01"></i>
+            <i class="iconfont icon-wjx01"></i>
+            <i class="iconfont icon-wjx02"></i>
+            <span class="attr">颜色：黑色 尺码：M</span>
+          </div>
+          <div class="text">网易云app上这款耳机非常不错 新人下载网易云购买这款耳机优惠大 而且耳机🎧确实正品 音质特别好 戴上这款耳机 听音乐看电影效果声音真是太棒了 无线方便 小盒自动充电 最主要是质量好音质棒 想要买耳机的放心拍 音效巴巴滴 老棒了</div>
+          <div class="time">
+            <span>2020-10-10 10:11:22</span>
+            <span class="zan"><i class="iconfont icon-dianzan"></i>100</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 <script>
@@ -134,6 +164,51 @@ export default {
       margin-left: 30px;
       &.active,&:hover {
         color: @xtxColor;
+      }
+    }
+  }
+   .list {
+    padding: 0 20px;
+    .item {
+      display: flex;
+      padding: 25px 10px;
+      border-bottom: 1px solid #f5f5f5;
+      .user {
+        width: 160px;
+        img {
+          width: 40px;
+          height: 40px;
+          border-radius: 50%;
+          overflow: hidden;
+        }
+        span {
+          padding-left: 10px;
+          color: #666;
+        }
+      }
+      .body {
+        flex: 1;
+        .score {
+          line-height: 40px;
+          .iconfont {
+            color: #ff9240;
+            padding-right: 3px;
+          }
+          .attr {
+            padding-left: 10px;
+            color: #666;
+          }
+        }
+      }
+      .text {
+        color: #666;
+        line-height: 24px;
+      }
+      .time {
+        color: #999;
+        display: flex;
+        justify-content: space-between;
+        margin-top: 5px;
       }
     }
   }
